1074103e74f3aa355eb9ea7e557941f10aa3061f
chan <cham9994@gmail.com>
2022-05-21 16:06:49 +0900

    220521 TIL: components

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>May 21th 2022</div>
+      <div>
+        <div>Car Insurance</div>
+        <div>$294.87</div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseItem
